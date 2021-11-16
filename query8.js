24454b823dd99bd37ef093c7af9404b3502e3d35
derekshao <junlis@umich.edu>
2021-11-15 22:15:29 -0500

    final

--- query8.js
+++ query8.js
@@ -4,23 +4,23 @@
 
 
 var city_average_friendcount_mapper = function () {
-  emit(this.hometown.city, { "user_cnt": 1, "friends_arr": this.friends });
+    emit(this.hometown.city, { "user_cnt": 1, "friends_arr": this.friends });
 };
 
 var city_average_friendcount_reducer = function (key, values) {
-  let user_cnt = 0;
-  let friends_arr = [];
-  for (let i = 0; i < values.length; ++i) {
-    user_cnt += values[i]["user_cnt"];
-    friends_arr = friends_arr.concat(values[i]["firends_arr"]);
-  }
-  return { "user_cnt": user_cnt, "friends_arr": friends_arr };
+    let user_cnt = 0;
+    let friends_arr_length = 0;
+    for (let i = 0; i < values.length; ++i) {
+        user_cnt += values[i]["user_cnt"];
+        friends_arr_length += values[i]["friends_arr"].length;
+    }
+    return { "user_cnt": user_cnt, "friends_arr": friends_arr };
 };
 
 var city_average_friendcount_finalizer = function (key, reduceVal) {
-  // We've implemented a simple forwarding finalize function. This implementation 
-  // is naive: it just forwards the reduceVal to the output collection.
-  // Feel free to change it if needed.
-  var ret = reduceVal.friends_arr.length / reduceVal.user_cnt;
-  return ret;
+    // We've implemented a simple forwarding finalize function. This implementation
+    // is naive: it just forwards the reduceVal to the output collection.
+    // Feel free to change it if needed.
+    var ret = reduceVal.friends_arr / reduceVal.user_cnt;
+    return ret;
 }
